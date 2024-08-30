a27f6fbc8d6ff17b97c435d6905909fb40cea709
AndersonReis04 <andersonaugustorei1227@gmail.com>
2024-08-29 21:38:16 -0300

    Dois exercicios

---
+++ Estruturas de dados/Arrays/criacaoeacesso.java
@@ -0,0 +1,22 @@
+public class criacaoeacesso{
+public static void main(String[] args) {
+    int[] numeros = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            numeros[i] = i + 1;
+        }
+        System.out.println(numeros[2]);
+        System.out.println(numeros[9]+1); 
+    }
+}
+
+
+
+
+
+/* 1. Criação e Acesso aos Elementos
+Crie um array com os números de 1 a 10.
+Acesse e imprima o terceiro elemento do array.
+Mude o valor do quinto elemento para 20.
+Adicione o número 11 ao final do array.
+*/
