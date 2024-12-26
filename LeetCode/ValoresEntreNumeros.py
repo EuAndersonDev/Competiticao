8c249ee6506f81761504d36b3538bc13095f0a89
AndersonReis04 <andersonaugustorei1227@gmail.com>
2024-12-26 19:31:31 -0300

    o

---
+++ LeetCode/ValoresEntreNumeros.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+if a > b:
+    count = a
+    menor = b
+else:
+    count = b
+    menor = a
+    
+list = []
+for i in range(i<count):
+    list.append(menor + i)
+
+list.pop()
+print(" ".join(map(str, list)))
